077930923d5c66d57a7a0589c3631e4123f91618
Tapir <196589554+Imatapir@users.noreply.github.com>
2026-07-16 22:48:53 -0400

    ajout de pages hors-terre et cressee, modif du CSS

--- ssg/public/services/piscine/creusee/index.php
+++ ssg/public/services/piscine/creusee/index.php
@@ -1,10 +1,67 @@
-<?php partial('head', ['title' => 'Contact']) ?>
+<?php partial('head', ['title' => 'Piscine hors-terre']) ?>
 
-<h1>Piscine Creusée</h1>
+<!-- <h1>Piscine Creusée</h1>
 
 <p>
     This page corresponds to
     <code>public/services/piscine/creusee/index.php</code>.
-</p>
+</p> -->
+
+
+
+
+<section class="piscine">
+    <div class="carousel">
+        <button class="carousel__btn carousel__btn--prev" aria-label="Image précédente">
+            <span class="carousel__arrow-outer carousel__arrow-outer--left">
+                <span class="carousel__arrow-inner carousel__arrow-inner--left"></span>
+            </span>
+        </button>
+        <div class="carousel__viewport">
+            <img src="/assets/images/horsterre01.avif" alt="Piscine hors-terre" class="carousel__image">
+        </div>
+        <button class="carousel__btn carousel__btn--next" aria-label="Image suivante">
+            <span class="carousel__arrow-outer carousel__arrow-outer--right">
+                <span class="carousel__arrow-inner carousel__arrow-inner--right"></span>
+            </span>
+        </button>
+    </div>
+
+    <div class="piscine__intro">
+        <h2 class="mainText piscine__titre">Solutions complètes pour piscines hors terre sur la Rive-Nord, à Laval et Blainville</h2>
+        <p class="text piscine__lead">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. AquaNord se spécialise en remplacement
+            de toile de piscine hors terre. Que vous soyez sur la Rive-Nord, à Laval ou à Blainville,
+            nos professionnels d'expérience se déplacent afin de répondre à tous vos besoins.
+        </p>
+        <p class="text piscine__lead">
+            Nous mettons à votre disposition une gamme de piscines hors terre durables et de qualité.
+            Nos experts évalueront vos besoins afin de vous proposer un modèle idéal (résine, hybride, acier).
+        </p>
+    </div>
+
+    <div class="piscine__services">
+        <h3 class="textTitles piscine__services-titre">Services pour piscines hors terre</h3>
+        <ul class="piscine__services-liste">
+            <li class="text piscine__services-item">Ouverture</li>
+            <li class="text piscine__services-item">Fermeture</li>
+            <li class="text piscine__services-item">Réparation et changement de toile</li>
+            <li class="text piscine__services-item">Détection de fuites</li>
+            <li class="text piscine__services-item">Entretien hebdomadaire (vidage et lavage)</li>
+        </ul>
+    </div>
+
+    <div class="piscine__approche">
+        <h4 class="textTitles piscine__approche-titre">Approche personnalisée</h4>
+        <p class="text piscine__approche-texte">
+            Chez AquaNord, nous prenons notre métier très au sérieux et nous nous assurons de vous offrir
+            un travail soigné, et ce, en tout temps. Notre approche personnalisée et nos conseils éclairés
+            vous permettront de profiter pleinement de votre piscine. N'hésitez pas à faire appel à nos
+            spécialistes afin de bénéficier d'une estimation gratuite pour tous vos travaux de piscine hors terre.
+        </p>
+    </div>
+</section>
 
 <?php partial('footer') ?>
+
+
--- ssg/public/services/piscine/hors_terre/index.php
+++ ssg/public/services/piscine/hors_terre/index.php
@@ -1,10 +1,48 @@
 <?php partial('head', ['title' => 'Contact']) ?>
 
-<h1>Piscine Hors-Terre</h1>
+<!-- <h1>Piscine Hors-Terre</h1>
 
 <p>
     This page corresponds to
     <code>public/services/piscine/hors_terre/index.php</code>.
-</p>
+</p> -->
+
+<section class="piscine">
+    <div class="piscine__image-wrapper">
+        <img src="/assets/images/creusee01.avif" alt="Piscine creusée" class="piscine__image">
+    </div>
+
+    <div class="piscine__intro">
+        <h2 class="mainText piscine__titre">Entretien de piscines creusées à Blainville, Laval et sur la Rive-Nord</h2>
+        <p class="text piscine__lead">
+            Lorsque vient le temps d'ouvrir ou de fermer votre piscine creusée dans la région de Blainville,
+            AquanNord est le partenaire qu'il vous faut. Si vous partez en voyage cet été, notre équipe
+            se charge de l'entretien de votre piscine. Vous serez heureux de profiter de votre piscine dès
+            votre retour! De plus, notre personnel est spécialisé en remplacement de toile de piscine.
+            Avec nous, vous pourrez avoir l'assurance que tout est fait correctement et que vos installations
+            de piscines creusées sont fonctionnelles.
+        </p>
+    </div>
+
+    <div class="piscine__services">
+        <h3 class="textTitles piscine__services-titre">Services pour piscines creusées</h3>
+        <ul class="piscine__services-liste">
+            <li class="text piscine__services-item">Ouverture</li>
+            <li class="text piscine__services-item">Fermeture</li>
+            <li class="text piscine__services-item">Réparation et changement de toile</li>
+            <li class="text piscine__services-item">Détection de fuites</li>
+            <li class="text piscine__services-item">Entretien hebdomadaire (vidage et lavage)</li>
+        </ul>
+    </div>
+
+    <div class="piscine__approche">
+        <h4 class="textTitles piscine__approche-titre">Un appel suffit</h4>
+        <p class="text piscine__approche-texte">
+            Pour toute question ou demande d'estimation, <a href="/contact/" class="piscine__lien">appelez-nous</a>!
+            Nous nous déplaçons à Blainville, à Laval et sur la Rive-Nord. Les membres de notre équipe
+            interviennent rapidement et peuvent même se déplacer pour les situations d'urgence.
+        </p>
+    </div>
+</section>
 
 <?php partial('footer') ?>
